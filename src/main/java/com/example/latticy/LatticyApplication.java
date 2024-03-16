a2db47da8606ba954dd60a00eeb62a67a99f2cd5
nui-Utal <1575178405@qq.com>
2024-03-16 15:37:56 +0800

    init

---
+++ src/main/java/com/example/latticy/LatticyApplication.java
@@ -0,0 +1,13 @@
+package com.example.latticy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatticyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatticyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/latticy/LatticyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latticy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatticyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
